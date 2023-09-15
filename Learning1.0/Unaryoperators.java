b284b3cc08838ca40d71a790f32611979cedd558
devaraj94 <102654090+devaraj94@users.noreply.github.com>
2023-09-15 09:49:04 +0530

    Add files via upload

---
+++ Learning1.0/Unaryoperators.java
@@ -0,0 +1,20 @@
+ public class Unaryoperators{
+ 
+ public static void main(String args[]){
+ int number=40;
+ 
+ 
+ System.out.println(number); //40
+ 
+ System.out.println(++number); //41
+
+
+
+System.out.println(number++); //41
+
+System.out.println(--number); //41
+
+System.out.println(number--);   //41
+
+ }
+ }
